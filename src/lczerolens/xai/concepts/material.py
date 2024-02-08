07c6294145a32709e19bb4903b9ee90941df9f1a
Xmaster6y <66315201+Xmaster6y@users.noreply.github.com>
2024-02-08 14:33:49 +0100

    material related concepts

--- src/lczerolens/xai/concepts/material.py
+++ src/lczerolens/xai/concepts/material.py
@@ -31,8 +31,42 @@ class HasPieceConcept(BinaryConcept):
         Compute the label for a given model and input.
         """
         if self.relative:
-            color = self.piece.color and board.turn
+            color = self.piece.color if board.turn else not self.piece.color
         else:
             color = self.piece.color
         squares = board.pieces(self.piece.piece_type, color)
-        return len(squares) > 0
+        return 1 if len(squares) > 0 else 0
+
+
+class HasThreatConcept(BinaryConcept):
+    """
+    Class for material concept-based XAI methods.
+    """
+
+    def __init__(
+        self,
+        piece: str,
+        relative: bool = True,
+    ):
+        """
+        Initialize the class.
+        """
+        self.piece = chess.Piece.from_symbol(piece)
+        self.relative = relative
+
+    def compute_label(
+        self,
+        board: chess.Board,
+    ) -> int:
+        """
+        Compute the label for a given model and input.
+        """
+        if self.relative:
+            color = self.piece.color if board.turn else not self.piece.color
+        else:
+            color = self.piece.color
+        squares = board.pieces(self.piece.piece_type, color)
+        for square in squares:
+            if board.is_attacked_by(not color, square):
+                return 1
+        return 0
