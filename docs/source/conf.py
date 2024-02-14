6f367b59ea6c49ef88fab65b9283ce7f1455701e
Xmaster6y <66315201+Xmaster6y@users.noreply.github.com>
2024-02-14 23:35:44 +0100

    version switcher

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+
+import lczerolens
+
 # Project Information
 project = "lczerolens"
 copyright = "2024, Yoann Poupart"
@@ -30,6 +34,28 @@ html_static_path = ["_static"]
 
 html_favicon = "_static/images/favicon.ico"
 html_show_sourcelink = False
+
+# Define the json_url for our version switcher.
+json_url = "https://lczerolens.readthedocs.io/en/latest/_static/switcher.json"
+
+
+version_match = os.environ.get("READTHEDOCS_VERSION")
+release = lczerolens.__version__
+# If READTHEDOCS_VERSION doesn't exist, we're not on RTD
+# If it is an integer, we're in a PR build and the version isn't correct.
+# If it's "latest" → change to "dev"
+if not version_match or version_match.isdigit() or version_match == "latest":
+    # For local development, infer the version to match from the package.
+    if "dev" in release or "rc" in release:
+        version_match = "dev"
+        # We want to keep the relative reference if we are in dev mode
+        # but we want the whole url if we are effectively in a released version
+        json_url = "_static/switcher.json"
+    else:
+        version_match = f"v{release}"
+elif version_match == "stable":
+    version_match = f"v{release}"
+
 html_theme_options = {
     "show_nav_level": 2,
     "navbar_end": ["theme-switcher", "navbar-icon-links"],
@@ -45,7 +71,20 @@ html_theme_options = {
             "url": "https://discord.gg/e7vhrTsjnt",
             "icon": "fa-brands fa-discord",
         },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/pydata-sphinx-theme",
+            "icon": "fa-custom fa-pypi",
+        },
     ],
+    "show_version_warning_banner": True,
+    "navbar_center": ["version-switcher", "navbar-nav"],
+    "footer_start": ["copyright"],
+    "footer_center": ["sphinx-version"],
+    "switcher": {
+        "json_url": json_url,
+        "version_match": version_match,
+    },
 }
 
 html_context = {"default_mode": "auto"}
