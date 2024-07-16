63ae6d5edcd4298837bd1cc331000f56dcc8f072
Yoann Poupart <yoann.poupart@ens-lyon.org>
2024-07-16 16:01:39 +0200

    kwargs

--- src/lczerolens/play/sampling.py
+++ src/lczerolens/play/sampling.py
@@ -53,7 +53,7 @@ class Sampler(ABC):
         idx = m.sample()
         return move_encodings.decode_move(legal_indices[idx], board)
 
-    def get_next_moves(self, boards: Iterable[chess.Board]) -> Iterable[Tuple[chess.Move, Dict[str, float]]]:
+    def get_next_moves(self, boards: Iterable[chess.Board], **kwargs) -> Iterable[Tuple[chess.Move, Dict[str, float]]]:
         """Get the next move.
 
         Parameters
@@ -67,7 +67,7 @@ class Sampler(ABC):
             The iterable over the moves and log dictionaries.
         """
         util_boards, move_boards = tee(boards)
-        for board, (utility, legal_indices, to_log) in zip(move_boards, self.get_utilities(util_boards)):
+        for board, (utility, legal_indices, to_log) in zip(move_boards, self.get_utilities(util_boards, **kwargs)):
             predicted_move = self.choose_move(board, utility, legal_indices)
             yield predicted_move, to_log
 
@@ -108,9 +108,9 @@ class ModelSampler(Sampler):
     def get_utilities(
         self, boards: Iterable[chess.Board], **kwargs
     ) -> Iterable[Tuple[torch.Tensor, torch.Tensor, Dict[str, float]]]:
-        batch_size = kwargs.get("batch_size", -1)
+        batch_size = kwargs.pop("batch_size", -1)
 
-        for legal_indices, batch_stats in self._get_batched_stats(boards, batch_size):
+        for legal_indices, batch_stats in self._get_batched_stats(boards, batch_size, **kwargs):
             to_log = {}
             utility = 0
             q_values = self._get_q_values(batch_stats, to_log)
@@ -120,12 +120,12 @@ class ModelSampler(Sampler):
             to_log["max_utility"] = utility.max().item()
             yield utility, legal_indices, to_log
 
-    def _get_batched_stats(self, boards, batch_size, use_next_boards=True):
+    def _get_batched_stats(self, boards, batch_size, use_next_boards=True, **kwargs):
         next_batch = []
         next_legal_indices = []
 
         def generator(next_batch, next_legal_indices):
-            all_stats = self.model(*next_batch)
+            all_stats = self.model(*next_batch, **kwargs)
             offset = 0
             for legal_indices in next_legal_indices:
                 n_boards = legal_indices.shape[0] + 1 if use_next_boards else 1
@@ -191,10 +191,10 @@ class PolicySampler(ModelSampler):
     def get_utilities(
         self, boards: Iterable[chess.Board], **kwargs
     ) -> Iterable[Tuple[torch.Tensor, torch.Tensor, Dict[str, float]]]:
-        batch_size = kwargs.get("batch_size", -1)
+        batch_size = kwargs.pop("batch_size", -1)
 
         to_log = {}
-        for legal_indices, batch_stats in self._get_batched_stats(boards, batch_size, use_next_boards=False):
+        for legal_indices, batch_stats in self._get_batched_stats(boards, batch_size, use_next_boards=False, **kwargs):
             legal_policy = batch_stats["policy"][0].gather(0, legal_indices.to(batch_stats["policy"].device))
             if self.use_suboptimal:
                 idx = legal_policy.argmax()
@@ -215,21 +215,27 @@ class SelfPlay:
         max_moves: int = 100,
         to_play: chess.Color = chess.WHITE,
         report_fn: Optional[Callable[[dict, chess.Color], None]] = None,
+        white_kwargs: Optional[Dict] = None,
+        black_kwargs: Optional[Dict] = None,
     ):
         """
         Plays a game.
         """
         if board is None:
             board = chess.Board()
+        if white_kwargs is None:
+            white_kwargs = {}
+        if black_kwargs is None:
+            black_kwargs = {}
         game = []
         if to_play == chess.BLACK:
-            move, _ = next(iter(self.black.get_next_moves([board])))
+            move, _ = next(iter(self.black.get_next_moves([board], **black_kwargs)))
             board.push(move)
             game.append(move)
         for _ in range(max_moves):
             if board.is_game_over() or len(game) >= max_moves:
                 break
-            move, to_log = next(iter(self.white.get_next_moves([board])))
+            move, to_log = next(iter(self.white.get_next_moves([board], **white_kwargs)))
             if report_fn is not None:
                 report_fn(to_log, board.turn)
             board.push(move)
@@ -237,7 +243,7 @@ class SelfPlay:
 
             if board.is_game_over() or len(game) >= max_moves:
                 break
-            move, to_log = next(iter(self.black.get_next_moves([board])))
+            move, to_log = next(iter(self.black.get_next_moves([board], **black_kwargs)))
             if report_fn is not None:
                 report_fn(to_log, board.turn)
             board.push(move)
