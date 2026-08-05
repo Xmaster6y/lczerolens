80a649608e5a87dc240068fb42bec42842b2f6a0
Yoann Poupart <66315201+Xmaster6y@users.noreply.github.com>
2026-08-05 18:54:55 +0900

    Fix v0.5.0 docs selector (#168)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,7 +65,9 @@ release = lczerolens.__version__
 # If READTHEDOCS_VERSION doesn't exist, we're not on RTD
 # If it is an integer, we're in a PR build and the version isn't correct.
 # If it's "latest" → change to "dev"
-if not version_match or version_match.isdigit() or version_match == "latest":
+if version_match == "latest":
+    version_match = "dev"
+elif not version_match or version_match.isdigit():
     # For local development, infer the version to match from the package.
     if "dev" in release or "rc" in release:
         version_match = "dev"
